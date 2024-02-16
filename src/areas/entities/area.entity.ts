1e9a9ace926c3f95092422be4c8400270cb3ff5d
mubinkg <mubin.ice.ru@gmail.com>
2024-02-16 10:16:16 +0600

    add areas schema

--- src/areas/entities/area.entity.ts
+++ src/areas/entities/area.entity.ts
@@ -1,7 +1,26 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Prop, Schema } from '@nestjs/mongoose';
 
 @ObjectType()
+@Schema()
 export class Area {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => String, { nullable: true})
+  @Prop({type: String})
+  area_name: String;
+
+  @Field(() => String, { nullable: true})
+  @Prop({type: String})
+  city_id: String;
+
+  @Field(() => String, { nullable: true})
+  @Prop({type: String})
+  zipcode_id: String;
+
+  @Field(() => Number, { nullable: true})
+  @Prop({type: Number})
+  minimum_free_delivery_order_amount: Number;
+
+  @Field(() => Number, { nullable: true})
+  @Prop({type: Number})
+  delivery_charges: Number;
 }
