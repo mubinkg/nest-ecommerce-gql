b47d07d29b7f1eb8a6def2f7aba44a3a0856490c
mubinkg <mubin.ice.ru@gmail.com>
2024-02-26 16:34:16 +0600

    add order schema

--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -1,7 +1,76 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type OrderDocument = HydratedDocument<Order>
 
 @ObjectType()
+@Schema()
 export class Order {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  
+  @Field(()=>String, {nullable:true})
+  @Prop({type: String})
+  user_id?:string
+
+  @Field(()=>String, {nullable:true})
+  @Prop({type:String})
+  mobile?:string
+
+  @Field(()=>[String], {nullable:true})
+  @Prop([{type:String}])
+  product_variant_id?:string[]
+
+  @Field(()=>[Number], {nullable:true})
+  @Prop([{type:Number}])
+  quantity?:number[]
+
+  @Field(()=>Number,{nullable:true})
+  @Prop({type: Number})
+  total?:number
+
+  @Field(()=>Number,{nullable:true})
+  @Prop({type: Number})
+  delivery_charge?:number
+
+  @Field(()=>Number,{nullable:true})
+  @Prop({type: Number})
+  final_total?:number
+
+  @Field(()=>String, {nullable:true})
+  @Prop({type:String})
+  promo_code?:string
+
+  @Field(()=>Number,{nullable:true})
+  @Prop({type: Number})
+  promo_discount?:number
+
+  @Field(()=>String, {nullable:true})
+  @Prop({type:String})
+  payment_method?:string
+
+  @Field(()=>String, {nullable:true})
+  @Prop({type:String})
+  address_id?:string
+
+  @Field(()=>Number, {nullable:true, defaultValue: 0})
+  @Prop({type:Number})
+  is_wallet_used?:number
+
+  @Field(()=>Number, {nullable:true, defaultValue: 0})
+  @Prop({type:Number})
+  wallet_balance_used?:number
+
+  @Field(()=>String, {nullable:true})
+  @Prop({type:String})
+  order_note?:string //{optional}
+
+  @Field(()=>[String], {nullable:true})
+  @Prop([{type:String}])
+  documents?:string[]
+
+  @Field(()=>String, {nullable:true})
+  @Prop({type:String})
+  email?:string // only enter when ordered product is digital product and one of them is not downloadable(download_allowed = 0)
 }
+
+export const OrderSchema = SchemaFactory.createForClass(Order)
