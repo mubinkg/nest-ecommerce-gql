80cc6402d038f4adc137280269aba0bcd4686bf5
mubin <mubin.ice.ru@gmail.com>
2024-03-09 14:53:29 +0600

    add category input dto

---
+++ src/categories/entities/get-category.dto.ts
@@ -0,0 +1,35 @@
+import { Field, InputType } from "@nestjs/graphql";
+import { IsBoolean, IsNumber, IsOptional, IsString } from "class-validator";
+
+@InputType()
+export class GetCategoryDto{
+    @Field(()=>String, {nullable:true})
+    @IsString()
+    @IsOptional()
+    id: string
+
+    @Field(()=>Number, {nullable:true})
+    @IsNumber()
+    @IsOptional()
+    limit?: number
+
+    @Field(()=>Number, {nullable:true})
+    @IsNumber()
+    @IsOptional()
+    offset?: number
+
+    @Field(()=>String, {nullable:true})
+    @IsString()
+    @IsOptional()
+    sort?: string
+
+    @Field(()=>String, {nullable:true})
+    @IsString()
+    @IsOptional()
+    order?:string
+
+    @Field(()=>Boolean, {nullable:true})
+    @IsString()
+    @IsBoolean()
+    has_child_or_item?: boolean
+}
