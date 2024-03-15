5ba14034a761736d7521d977b6fdbfa2dcdd69db
mubin <mubin.ice.ru@gmail.com>
2024-03-16 00:42:59 +0600

    add faq shema

--- src/faq/entities/faq.entity.ts
+++ src/faq/entities/faq.entity.ts
@@ -1,7 +1,26 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Prop, Schema } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+import { Product } from 'src/products/entities/product.entity';
 
 @ObjectType()
+@Schema({
+  timestamps: true
+})
 export class Faq {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(()=>Product, {nullable:true})
+  @Prop({type:mongoose.Types.ObjectId, ref: 'Product'})
+  product?: Product
+
+  @Field(()=>String, {nullable:true})
+  @Prop({type: String})
+  question?: string
+
+  @Field(()=>String, {nullable:true})
+  @Prop({type: mongoose.Types.ObjectId})
+  userId?: string
+
+  @Field(()=>String, {nullable:true})
+  @Prop({type: String})
+  ans?: string
 }
