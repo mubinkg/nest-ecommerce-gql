fc7bdbcbab870ef7969e16f53a4109a31cd99e96
mubin <mubin.ice.ru@gmail.com>
2024-03-25 00:05:32 +0600

    update port

--- src/aramics/aramics.service.ts
+++ src/aramics/aramics.service.ts
@@ -1,11 +1,95 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAramicInput } from './dto/create-aramic.input';
 import { UpdateAramicInput } from './dto/update-aramic.input';
+import axios from 'axios';
 
 @Injectable()
 export class AramicsService {
-  create(createAramicInput: CreateAramicInput) {
-    return 'This action adds a new aramic';
+  async create(createAramicInput: CreateAramicInput) {
+    try{
+      await axios.post('wms.acieshop.com/api/orderNomalRegist',[{
+        "Departure_Station": "SEL",
+        "Arrival_Nation": "US",
+        "Transfer_Company_Code": "",
+        "Order_Date": "20220922",
+        "Order_Number": "20201117101799",
+        "Hawb_No": "",
+        "Shipper_Name": "ACI EXPRESSFF (U.K) LTD.",
+        "Shipper_Country": "KR",
+        "Shipper_State": "",
+        "Shipper_City": "SEL",
+        "Shipper_Zip": "07641",
+        "Shipper_Address": "Dubai",
+        "Shipper_Address_Detail": "Hall town Dubai",
+        "Shipper_Tel": "+8210976543",
+        "Shipper_Hp": "",
+        "Shipper_Email": "",
+        "Receiver_Country": "US",
+        "Receiver_State": "New York",
+        "Receiver_City": "New York",
+        "Receiver_District": "",
+        "Receiver_Zip": "12401",
+        "Receiver_Name": "Kamlesh kumar",
+        "Native_Receiver_Name": "Native",
+        "Receiver_Address_Detail": "",
+        "Receiver_Tel": "+8210976543",
+        "Receiver_Hp": "",
+        "Receiver_Email": "",
+        "Box_Count": "1",
+        "Actual_Weight": "2.6",
+        "Volume_Weight": "",
+        "Volume_Length": "",
+        "Volume_Width": "",
+        "Volume_Height": "",
+        "Custom_Clearance_ID": "",
+        "Buy_Site": "https://kocart.com/",
+        "Size_Unit": "CM",
+        "Weight_Unit": "KG",
+        "Get_Buy": "1",
+        "Mall_Type": "A",
+        "Warehouse_Msg": "",
+        "Delivery_Msg": "Call M",
+        "Exp_Licence_YN": "N",
+        "Exp_Business_Num": "",
+          "Item_Material":[
+              {
+                  "Customer_Item_Code": "ITEM2",
+                  "Hs_Code": "",
+                  "Brand": "Handcraft Vietnam1",
+                  "Item_Detail": "(Handcraft Vietnam) Straw Tote Bag",
+                  "Native_Item_Detail": "75301-6-1 VOGACORTE 70",
+                  "Item_Cnt": "1",
+                  "Unit_Value": "20.5",
+                  "Make_Country": "GB",
+                  "Make_Company": "",
+                  "Item_Div": "",
+                  "Qty_Unit": "EA",
+                  "Item_Url": "TESTURL.COM",
+                  "Item_Img_Url": "",
+                  "Trking_Company": "EPOST",
+                  "Trking_Number": "6063412344789",
+                  "Trking_Date": "20220921",
+                  "Chg_Currency": "USD",
+                  "Item_Material": ""
+              }
+          ]
+      }]
+      ,
+      {
+        headers: {
+          'Content-Type':'application/json',
+          "UserID": "eresitrade",
+          "APIkey":"M6sCqNy4tq4F4bGq/J51xkuYv+6qJ68Rdd8bW0lUsIQ="
+        }
+      }
+      )
+      return {
+        "exampleField": 1
+      }
+    }
+    catch(err){
+      throw err;
+    }
   }
 
   findAll() {
--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
   app.use(graphqlUploadExpress({maxFileSize:1000*1000*1000, maxFiles: 5}))
-  await app.listen(3000);
-  console.log('App started on http://localhost:3000/graphql')
+  await app.listen(3001);
+  console.log('App started on http://localhost:3001/graphql')
 }
 bootstrap();
