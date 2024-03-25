f13371ca3c9415add4c626bce3f1a60199582699
mubin <mubin.ice.ru@gmail.com>
2024-03-26 01:28:14 +0600

    add seller schema

--- src/sellers/entities/seller.entity.ts
+++ src/sellers/entities/seller.entity.ts
@@ -1,7 +1,58 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field } from '@nestjs/graphql';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
 
+@Schema()
 @ObjectType()
 export class Seller {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => String, {nullable:true})
+  @Prop({type: String})
+  name?: String;
+
+  @Field(() => String, {nullable:true})
+  @Prop({type: String})
+  mobile?: String;
+
+  @Field(() => String, {nullable:true})
+  @Prop({type: String})
+  email?: String;
+
+  @Field(() => String, {nullable:true})
+  @Prop({type: String})
+  password?: String;
+
+  @Field(() => String, {nullable:true})
+  @Prop({type: String})
+  address?: String;
+
+  @Field(() => String, {nullable:true})
+  @Prop({type: String})
+  address_proof?: String;
+
+  @Field(() => String, {nullable:true})
+  @Prop({type: String})
+  national_identity_card?: String;
+
+  @Field(() => String, {nullable:true})
+  @Prop({type: String})
+  business_license?: String;
+
+  @Field(() => String, {nullable:true})
+  @Prop({type: String})
+  account_number?: String;
+
+  @Field(() => String, {nullable:true})
+  @Prop({type: String})
+  account_name?: String;
+
+  @Field(() => String, {nullable:true})
+  @Prop({type: String})
+  bank_code?: String;
+
+  @Field(() => String, {nullable:true})
+  @Prop({type: String})
+  bank_name?: String;
 }
+
+export type SellerDocument = HydratedDocument<Seller>
+export const SellerSchema = SchemaFactory.createForClass(Seller)
