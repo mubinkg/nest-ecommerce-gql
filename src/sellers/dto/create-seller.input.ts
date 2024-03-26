379b5af1fd7f2092b9625c96b2dc15e1edd52afc
mubin <mubin.ice.ru@gmail.com>
2024-03-26 13:34:34 +0600

    add seller registration schema

--- src/sellers/dto/create-seller.input.ts
+++ src/sellers/dto/create-seller.input.ts
@@ -1,7 +1,61 @@
 import { InputType, Int, Field } from '@nestjs/graphql';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { FileUpload, GraphQLUpload } from 'graphql-upload';
 
 @InputType()
 export class CreateSellerInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  
+  @Field(() => String)
+  @IsString()
+  @IsNotEmpty()
+  name: String;
+
+  @Field(() => String)
+  @IsString()
+  @IsNotEmpty()
+  mobile: String;
+
+  @Field(() => String)
+  @IsString()
+  @IsNotEmpty()
+  email: String;
+
+  @Field(() => String)
+  @IsString()
+  @IsNotEmpty()
+  password: String;
+
+  @Field(() => String)
+  @IsString()
+  @IsNotEmpty()
+  address: String;
+
+  @Field(() => GraphQLUpload, {nullable:true})
+  address_proof?: FileUpload;
+
+  @Field(() => GraphQLUpload, {nullable:true})
+  national_identity_card?: FileUpload;
+
+  @Field(() => GraphQLUpload, {nullable:true})
+  business_license?: FileUpload;
+
+  @Field(() => String)
+  @IsString()
+  @IsNotEmpty()
+  account_number: String;
+
+  @Field(() => String)
+  @IsString()
+  @IsNotEmpty()
+  account_name: String;
+
+  @Field(() => String)
+  @IsString()
+  @IsNotEmpty()
+  bank_code: String;
+
+  @Field(() => String)
+  @IsString()
+  @IsNotEmpty()
+  bank_name: String;
 }
