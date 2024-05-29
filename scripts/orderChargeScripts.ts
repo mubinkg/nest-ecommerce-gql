ec04fb815b1deffaa85438c6c204f1d6946bbb42
mubin <mubin.ice.ru@gmail.com>
2024-05-30 00:24:02 +0600

    add delevery charge schema

---
+++ scripts/orderChargeScripts.ts
@@ -0,0 +1,28 @@
+const { MongoClient } = require('mongodb');
+
+const readXlsxFile = require('read-excel-file/node')
+
+readXlsxFile("../data/data.xlsx").then((rows) => {
+    const url = 'mongodb://localhost:27017';
+    const client = new MongoClient(url);
+    const dbName = 'cocart';
+    const data = []
+    for(let i = 3;i<rows.length;i++){
+        for(let j=1;j<rows[i].length;j++){
+            const countryCode = rows[0][j]
+            const countryName = rows[1][j]
+            const eramicsCountryName = rows[2][j]
+            const weight = rows[i][0]
+            const price = parseFloat(rows[i][j]?.split(",")?.join(""))?.toFixed(3) || null
+
+            data.push({countryCode, countryName, eramicsCountryName, weight, price})
+        }
+    }
+    client.connect().then(()=>{
+        const db = client.db(dbName)
+        const collection = db.collection('deliverycharges');
+        collection.insertMany(data).then(()=>{
+            console.log('Insert Done')
+        })
+    })
+  })
