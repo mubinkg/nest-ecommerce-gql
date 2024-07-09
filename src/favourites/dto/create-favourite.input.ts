a1fd910f036a2aeaffd9f713b87735259165be16
mubin <mubin.ice.ru@gmail.com>
2024-07-09 06:55:12 +0600

    update validation

--- src/favourites/dto/create-favourite.input.ts
+++ src/favourites/dto/create-favourite.input.ts
@@ -1,11 +1,18 @@
 import { InputType, Int, Field } from '@nestjs/graphql';
+import { IsMongoId, IsNotEmpty, IsString } from 'class-validator';
 import mongoose from 'mongoose';
 
 @InputType()
 export class CreateFavouriteInput {
   @Field(()=>String, {nullable:true})
+  @IsString()
+  @IsMongoId()
+  @IsNotEmpty()
   product_id: string
 
   @Field(()=>String, {nullable:true})
+  @IsString()
+  @IsMongoId()
+  @IsNotEmpty()
   user_Id: string
 }
