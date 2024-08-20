b8568a4499688e63f8f84c92a8fab7745fa389b7
mubin <mubin.ice.ru@gmail.com>
2024-08-21 02:36:01 +0600

    add flash sell entity

--- src/flash-sell/entities/flash-sell.entity.ts
+++ src/flash-sell/entities/flash-sell.entity.ts
@@ -1,7 +1,25 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Prop, Schema } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+import { Product } from 'src/products/entities/product.entity';
 
 @ObjectType()
+@Schema({
+  timestamps: true
+})
 export class FlashSell {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(()=>String)
+  _id: string
+
+  @Field(()=>Product, {nullable:true})
+  @Prop({type:mongoose.Schema.Types.ObjectId, ref: "Product"})
+  product?: Product
+
+  @Field(()=>Date, {nullable:true})
+  @Prop({type:Date})
+  endTime?: Date
+
+  @Field(()=>String, {nullable:true})
+  @Prop({type:String})
+  status?: string
 }
