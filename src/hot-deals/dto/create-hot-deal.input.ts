b46e431075980194771e9fd1d14f643a46886487
mubin <mubin.ice.ru@gmail.com>
2024-08-23 00:54:07 +0600

    add hot deals create dto

--- src/hot-deals/dto/create-hot-deal.input.ts
+++ src/hot-deals/dto/create-hot-deal.input.ts
@@ -1,7 +1,15 @@
 import { InputType, Int, Field } from '@nestjs/graphql';
+import { IsArray, IsDate, IsNotEmpty, IsString } from 'class-validator';
 
 @InputType()
 export class CreateHotDealInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @IsArray()
+  @IsNotEmpty()
+  @Field(()=>[String])
+  products:string[]
+
+  @Field(()=>Date)
+  @IsDate()
+  @IsNotEmpty()
+  validTime:Date
 }
