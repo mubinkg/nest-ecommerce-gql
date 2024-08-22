d0fe5468f2d00fb984ba9096ff59098bfb43ea87
mubin <mubin.ice.ru@gmail.com>
2024-08-23 00:17:09 +0600

    add hot deals

--- src/hot-deals/entities/hot-deal.entity.ts
+++ src/hot-deals/entities/hot-deal.entity.ts
@@ -1,7 +1,24 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Prop, Schema } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+import { Product } from 'src/products/entities/product.entity';
 
 @ObjectType()
+@Schema({
+  timestamps:true
+})
 export class HotDeal {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => String)
+  _id: string;
+
+  @Field(()=>Product, {nullable:true})
+  @Prop({type:mongoose.Schema.Types.ObjectId, ref: "Product"})
+  product?:Product
+
+  @Field(()=>Date, {nullable:true})
+  @Prop({type: Date})
+  validTime?:Date
+
+  @Field(()=>Date, {nullable:true})
+  createdAt?:Date
 }
