66a8c9e9f9bd7ab5aae6a706b338afca44be9694
mubinkg <mubin.ice.ru@gmail.com>
2024-11-18 23:29:44 +0600

    udpate offer validator

--- src/offers/dto/create-offer.input.ts
+++ src/offers/dto/create-offer.input.ts
@@ -1,6 +1,6 @@
 import { InputType, Int, Field } from '@nestjs/graphql';
 import { OfferType } from '../enum';
-import { IsArray, IsEnum, IsMongoId, IsNotEmpty, IsString } from 'class-validator';
+import { IsArray, IsEnum, IsMongoId, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 @InputType()
 export class CreateOfferInput {
@@ -14,12 +14,19 @@ export class CreateOfferInput {
   @IsNotEmpty()
   image:string
 
-  @Field(()=>String)
+  @Field(()=>String, {nullable:true})
+  @IsOptional()
   @IsMongoId()
   @IsNotEmpty()
   category:string
 
-  @Field(()=>[String])
+  @Field(()=>String, {nullable:true})
+  @IsOptional()
+  @IsString()
+  link:string
+
+  @Field(()=>[String], {nullable:true})
+  @IsOptional()
   @IsArray()
   products:string[]
 }
