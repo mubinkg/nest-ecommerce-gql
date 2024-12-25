fbc8dc302dbd57e107d744130589c1f16a5ce14e
mubinkg <mubin.ice.ru@gmail.com>
2024-12-26 00:07:09 +0600

    add notification schema

--- src/notification/entities/notification.entity.ts
+++ src/notification/entities/notification.entity.ts
@@ -1,7 +1,15 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
 
 @ObjectType()
+@Schema({
+  timestamps:true
+})
 export class Notification {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => String)
+  _id: string;
 }
+
+export const NotificationSchema = SchemaFactory.createForClass(Notification)
+export type NotificationDocument = HydratedDocument<Notification>
--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { NotificationService } from './services/notification.service';
 import { NotificationResolver } from './resolvers/notification.resolver';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Notification, NotificationSchema } from './entities/notification.entity';
 
 @Module({
+  imports:[
+    MongooseModule.forFeature([
+      {
+        schema: NotificationSchema,
+        name: Notification.name
+      }
+    ])
+  ],
   providers: [NotificationResolver, NotificationService],
 })
 export class NotificationModule {}
