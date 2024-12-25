70c354842a3f671410378e3e133717740c0bbe5a
mubinkg <mubin.ice.ru@gmail.com>
2024-12-25 13:03:24 +0600

    add type

--- src/product-attributes/dto/attribute-set-values.dto.ts
+++ src/product-attributes/dto/attribute-set-values.dto.ts
@@ -1,4 +1,5 @@
 import { Field, ObjectType } from "@nestjs/graphql";
+import { ProductAttributeValueType } from "../enum/attribute-value-type.enum";
 
 @ObjectType()
 export class AttributeSetValuesDto{
@@ -16,6 +17,9 @@ export class AttributeSetValuesDto{
 class AttirbuteSetValue{
     @Field(()=>String, {nullable:true})
     _id?: string
+
+    @Field(()=>ProductAttributeValueType,{nullable:true})
+    type?:ProductAttributeValueType
 
     @Field(()=>String, {nullable:true})
     valueName?: string
